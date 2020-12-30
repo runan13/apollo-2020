86db9ed89cd7d0bd950b629542f2c27f10bbf3a2
runan13 <65391973+runan13@users.noreply.github.com>
2020-12-30 16:35:58 +0900

    GET MOVIE Query from GraphQL

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,20 @@
-export default () => "Home";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    return "Loading...";
+  }
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1>{m.id}</h1>);
+  }
+};
