28272f233d08a7c870e8e6e276f90234729df00b
runan13 <65391973+runan13@users.noreply.github.com>
2020-12-30 17:25:01 +0900

    GET MOVIE and MOVIES Query from GraphQL

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,28 @@
-export default () => "Detail";
+import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIE = gql`
+  ## for Apollo
+  query getMovie($id: Int!) {
+    ## GraphQL Query
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+export default () => {
+  const { id } = useParams(); // Param 으로 받아오는 인자값들은 모두 문자열이다. 따라서 Int를 받아와도 문자열로 저장 된다
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: +id },
+  });
+  if (loading) {
+    return "Loading...";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
+};
