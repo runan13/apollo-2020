3edc8631267ba2ab6477bd50ee306a4b56c48e5c
runan13 <65391973+runan13@users.noreply.github.com>
2020-12-30 17:44:14 +0900

    Styling

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,28 +1,66 @@
 import { useParams } from "react-router-dom";
 import { gql, useQuery } from "@apollo/client";
+import styled from "styled-components";
 
 const GET_MOVIE = gql`
-  ## for Apollo
   query getMovie($id: Int!) {
-    ## GraphQL Query
     movie(id: $id) {
-      id
       title
       medium_cover_image
+      language
+      rating
       description_intro
     }
   }
 `;
 
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+`;
+
 export default () => {
-  const { id } = useParams(); // Param 으로 받아오는 인자값들은 모두 문자열이다. 따라서 Int를 받아와도 문자열로 저장 된다
+  const { id } = useParams();
   const { loading, data } = useQuery(GET_MOVIE, {
-    variables: { id: +id },
+    variables: { id },
   });
-  if (loading) {
-    return "Loading...";
-  }
-  if (data && data.movie) {
-    return data.movie.title;
-  }
+  return (
+    <Container>
+      <Column>
+        <Title>Name</Title>
+        <Subtitle>English · 4.5</Subtitle>
+        <Description>lorem ipsum lalalla </Description>
+      </Column>
+      <Poster></Poster>
+    </Container>
+  );
 };
